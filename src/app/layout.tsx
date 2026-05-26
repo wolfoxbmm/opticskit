98b673433bc6a8129bf16e7b01edbe8d7a9a3341
wolfoxbmm <wolfoxbmm@users.noreply.github.com>
2026-05-26 14:51:54 +0800

    Add Umami analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;650;700&display=swap" rel="stylesheet" />
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="e5d4a374-05f1-43da-bf5a-466bb2e7b40b"></script>
       </head>
       <body className="min-h-full flex flex-col bg-[#F2F3F5] text-[#1A1A2E]">
         {children}
